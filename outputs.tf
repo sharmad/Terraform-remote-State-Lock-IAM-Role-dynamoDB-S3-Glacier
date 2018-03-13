951d1d39f69c6c89191057ae15052544e961f7fe
sharmad <devthedev1@gmail.com>
2018-03-13 16:17:34 +0000

    id instead of name

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 
 output "state_bucket" {
-  value = "${aws_s3_bucket.state_bucket.bucket}"
+  value = "${aws_s3_bucket.state_bucket.id}"
 }
 
 output "state_logs_bucket" {
-  value = "${aws_s3_bucket.logs_bucket.bucket}"
+  value = "${aws_s3_bucket.logs_bucket.id}"
 }
 
 output "state_lock_dynamodb_table" {
-  value = "${aws_dynamodb_table.terraform_state_lock.name}"
+  value = "${aws_dynamodb_table.terraform_state_lock.id}"
 }
